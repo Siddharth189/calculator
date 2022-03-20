8a701f392bab5249bacddaa40589fbb207d70bfc
Siddharth Singh <siddharthiiitg@gmail.com>
2022-03-21 00:02:16 +0530

    Added initial files 🔥🔥

---
+++ script.js
@@ -0,0 +1,106 @@
+let buffer = "0";
+let runningTotal = 0;
+let previousOperator = null;
+const screen = document.querySelector(".screen");
+
+
+document.querySelector(".toggle").addEventListener('click',(event)=>{
+    if(document.body.style.backgroundColor == "" || document.body.style.backgroundColor == "white"){
+        document.body.style.backgroundColor = "black";
+        document.querySelector('.toggle').style.backgroundColor = "white"; 
+        document.querySelector('.toggle').style.color = "black"; 
+        document.querySelector('.toggle').style.border = "2px solid white"; 
+    }else{
+        document.body.style.backgroundColor = "white";
+        document.querySelector('.toggle').style.backgroundColor = "black"; 
+        document.querySelector('.toggle').style.color = "white"; 
+        document.querySelector('.toggle').style.border = "2px solid black";
+    }
+});
+
+
+
+document.querySelector(".cal-buttons").addEventListener('click',(event)=>{
+    handleEvent(event.target.innerText);
+});
+
+
+function handleEvent(value) {
+    if(isNaN(parseInt(value))){
+        handleSymbol(value);
+    }else{
+        handleNumber(value);
+    }
+    rerender();
+}
+
+function handleNumber(value) {
+    if(buffer == '0'){
+        buffer = value;
+    }else{
+        buffer += value;
+    }
+
+}
+
+function handleSymbol(value) {
+    switch (value) {
+        case "C":
+            buffer = "0";
+            runningTotal = 0;
+            previousOperator = null;
+            break;
+        
+        case "=":
+            if(previousOperator === null){
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            buffer = "" + runningTotal;
+            runningTotal = 0;
+            break;
+        
+        case "🠔":
+            if(buffer.length === 1) {
+                buffer = "0";
+            }else{
+                buffer = buffer.substring(0,buffer.length-1);
+            }
+            break;
+        
+        default:
+            handleMath(value);
+            break;
+    }
+}
+
+function handleMath(value) {
+    const intBuffer = parseInt(buffer);
+
+    if(runningTotal === 0){
+        runningTotal = intBuffer;
+    }else{
+        flushOperation(intBuffer);
+    }
+
+    previousOperator = value;
+
+    buffer = "0";
+}
+
+function flushOperation(intBuffer) {
+    if(previousOperator === "+"){
+        runningTotal += intBuffer;
+    }else if(previousOperator === "-"){
+        runningTotal -= intBuffer;
+    }else if(previousOperator === "x"){
+        runningTotal *= intBuffer;
+    }else if(previousOperator === "÷"){
+        runningTotal /= intBuffer;
+    }
+}
+
+function rerender() {
+
+    screen.innerText = buffer;
+}
